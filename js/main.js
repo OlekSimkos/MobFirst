de7726f39d40aff76fdb03bb85c5b58aba534d24
Olek <simkos72@gmail.com>
2024-12-10 16:53:18 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function () {
+  $(".top-slider").slick({
+    dots: true,
+
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow:
+      '<button type="button" class="slick-arrow slick-next"> <img src="/mobFirst/images/next-arrow.svg" alt="next"></button>',
+    prevArrow:
+      '<button type="button" class="slick-arrow slick-prev"> <img src="/mobFirst/images/prev-arrow.svg" alt="prev"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {},
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
